52c7f48f16ee6e6c8ee6ae984050e6bb0bf00b6f
Josh Fjelstul <joshfjelstul@gmail.com>
2020-08-18 20:27:53 -0500

    Update documentation

--- R/opinions.R
+++ R/opinions.R
@@ -3,19 +3,17 @@
 # euaid R package
 ###########################################################################
 
-#' Comments on TRIS Notifications
+#' Opinions on TRIS Notifications
 #'
-#' A dataset of comments submitted by the European Commission, EU member states,
-#' and the Surveillence Authority of the European Free Trade Association (EFTA) in
-#' response to notifications from member states and third-party countries with
-#' access to the EU single market about proposed technical regulations as part of
-#' the 2015/1535 procedure.
+#' A dataset of detailed opinions submitted by the European Commission and EU
+#' member states in response to notifications from member states about proposed
+#' technical regulations as part of the 2015/1535 procedure.
 #'
 #' @docType data
 #'
-#' @usage comments
+#' @usage opinions
 #'
-#' @format A tibble with 9 variables and 10,503 observations:
+#' @format A tibble with 9 variables and 4,657 observations:
 #' \describe{
 #'
 #' \item{\code{key_ID}}{Numeric. An ID number that uniquely identifies each
@@ -44,24 +42,22 @@
 #' \item{\code{end_year}}{Numeric. The year the 2015/1535 procedure was
 #' concluded.}
 #'
-#' \item{\code{comment_ID}}{String. An ID number that uniquely identifies each
-#' comment in the format \code{TRIS:####:####:XX:C:YY}, where the first set of
+#' \item{\code{opinion_ID}}{String. An ID number that uniquely identifies each
+#' opinion in the format \code{TRIS:####:####:XX:O:YY}, where the first set of
 #' digits is the year the proposed technical regulation was notified, the second
 #' set of digits is the number of the notification, \code{XX} is a 2-character
 #' code for the member state or third-party country that notified the proposed
-#' technical regulation, \code{C} is a 1-character code indicating a comment (as
-#' opposed to a detailed opinion), and \code{YY} is a 2-character code for the
-#' EU member state that submitted the comment. Note that \code{YY} can also be
-#' \code{EC} for the European Commission and \code{EFTA} for the Surveillance
-#' Authority of the European Free Trade Association (EFTA).}
+#' technical regulation, \code{O} is a 1-character code indicating an opinion
+#' (as opposed to a comment), and \code{YY} is a 2-character code for the EU
+#' member state that submitted the opinion. Note that \code{YY} can also be
+#' \code{EC} for the European Commission.}
 #'
 #' \item{\code{comment_by}}{String. The name of the entity that submitted the
-#' comment. Either the Commission, the name of an EU member state, or the
-#' Surveillance Authority of the European Free Trade Association (EFTA).}
+#' opinion. Either the Commission or the name of an EU member state.}
 #'
 #' }
 #'
-"comments"
+"opinions"
 
 ###########################################################################
 # end R script
